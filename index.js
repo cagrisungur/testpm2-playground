00bb2ce9024a09c0f1ee2627d8815c315af9c9e6
cagri <cagri@90pixel.com>
2021-11-03 17:55:41 +0300

    node modul

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+console.log('test')
  
 app.listen(3000)
--- shipitfile.js
+++ shipitfile.js
@@ -3,12 +3,12 @@ module.exports = (shipit) => {
 
     shipit.initConfig({
         test: {
-            branch: 'test',
+            branch: 'main',
             workspace: '/home/v2/',
             deployTo: '/home/v2/',
-            repositoryUrl: 'ssh://git@gitlab.90pixel.net:3795/yaziyorumpro-v2/demo.git',
-            servers: 'root@206.189.61.230',
-            ignores: ['node_modules'],
+            repositoryUrl: 'git@github.com:cagrisungur/testpm2-playground.git',
+            servers: 'root@159.89.20.165',
+            //ignores: ['node_modules'],
             keepReleases: 2,
             key: '~/.ssh/id_rsa',
             env: 'prod'
